5dd6d05ff9e77b05307dd84562a0e0e97de58277
leonel <leonelmmorgado@gmail.com>
2026-08-10 21:11:03 -0300

    xoroshiro

--- prng.h
+++ prng.h
@@ -14,8 +14,6 @@
 #define PRNG_H
 #include <stdint.h>
 
-#define SEED_CONST 0x9E3779B97F4A7C15
-
 typedef struct _prng_state {
 	uint64_t s[4];
 } Prng_state;
@@ -24,31 +22,27 @@ void prng_init(Prng_state *state, uint64_t seed);
 uint64_t prng_gen(Prng_state *state);
 
 #ifdef PRNG_IMPLEMENTATION
-uint64_t _seed_prng(uint64_t seed) {
-	uint64_t result = seed + SEED_CONST;
+uint64_t _seed_prng(uint64_t *seed) {
+	uint64_t result = (*seed += 0x9E3779B97F4A7C15);
 	result = (result ^ (result >> 30)) * 0xBF58476D1CE4E5B9;
 	result = (result ^ (result >> 27)) * 0x94D049BB133111EB;
 	return result ^ (result >> 31);
 }
 
 void prng_init(Prng_state *state, uint64_t seed) {
-	uint64_t tmp = _seed_prng(seed);
-	state->s[0] = (uint32_t)tmp;
-	state->s[1] = (uint32_t)(tmp >> 32);
-
-	seed += SEED_CONST;
-	tmp = _seed_prng(seed);
-	state->s[2] = (uint32_t)tmp;
-	state->s[3] = (uint32_t)(tmp >> 32);
+	state->s[0] = _seed_prng(&seed);
+	state->s[1] = _seed_prng(&seed);
+	state->s[2] = _seed_prng(&seed);
+	state->s[3] = _seed_prng(&seed);
 }
 
 uint64_t _rol64(uint64_t x, int k) {
-	return (x << k) | (k >> (64 - k));
+	return (x << k) | (x >> (64 - k));
 }
 
 uint64_t prng_gen(Prng_state *state) {
 	uint64_t *s = state->s;
-	uint64_t const result = s[0] + s[3];
+	uint64_t const result = _rol64(s[1] * 5, 7) * 9;
 	uint64_t const t = s[1] << 17;
 
 	s[2] ^= s[0];
@@ -61,6 +55,7 @@ uint64_t prng_gen(Prng_state *state) {
 
 	return result;
 }
+
 #endif //PRNG_IMPLEMENTATION
 
 #endif //PRNG_H
